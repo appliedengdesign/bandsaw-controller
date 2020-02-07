d997347b41dbea48d8fb1fc754f13787d2825195
Mike Centola <mcentola@appliedengdesign.com>
2020-02-07 13:55:58 -0500

    Wrote getMotorTemp function
    Signed-off-by: Mike Centola <mcentola@appliedengdesign.com>

--- app.ino
+++ app.ino
@@ -119,9 +119,25 @@ float getOutputCurrent() {
 }
 
 float getMotorTemp() {
+    int i;
+    int sval = 0;
+
+    for (i = 0; i < 5; i++) {
+        sval = sval + analogRead(MOTOR_TEMP);
+    }
+
+    // Take average of 5 readings and convert to volts
+    float svalavg = sval / 5.0;
+    float volts = svalavg * 5.0;
+    volts /= 1024.0;
+
+    // Convert to temperature
+    float tempC = (volts - 0.5) * 100;
+
+    return tempC;
 
 }
 
 float getElectronicsTemp() {
-    
+
 }
